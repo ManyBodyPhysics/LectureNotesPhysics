7e22c2c35c4ec451b1d5600abd1d5107c31522bb
Heiko Hergert <hergert@nscl.msu.edu>
2016-10-07 00:29:18 -0400

    Fixes a bug in the diagonal part of the pairing Hamiltonian.

--- doc/src/Chapter10-programs/f95/pairing.f90
+++ doc/src/Chapter10-programs/f95/pairing.f90
@@ -522,8 +522,9 @@ PROGRAM  pairing
      ENDDO
   ENDDO
   !     then the diagonal 3body unperturbed part
-  DO i=1,pq_confs
+  DO i=1,3
      toper(i,i) = delta*i*2.d0
+     toper(i+3,i+3) = delta*(i+2)*2.d0
   ENDDO
   hoper = toper+voper
   WRITE(6,*) 'Hamiltonian matrix'
